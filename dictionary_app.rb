3022bcaf7527d75d0f4333bd2acb25203d08e2b2
Chau Pham <theonlychau@gmail.com>
2017-11-09 19:32:05 -0600

    add text

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,7 +1,7 @@
 require "unirest"
 
 system "clear"
-puts "Welcome to the Dictionary App!
+puts "Welcome to the Dictionary App!"
 
 puts "Enter a word: "
 input_word = gets.chomp
@@ -9,5 +9,6 @@ input_word = gets.chomp
 response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
 body = response.body
+definition = body["text"]
 
-puts body
+puts definition
