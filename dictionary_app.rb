62016543ff343e89bb3de7929c9ec5061e97d7ed
Chau Pham <theonlychau@gmail.com>
2017-11-09 20:25:13 -0600

    Peter's code

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,24 +7,23 @@ puts "Enter a word: "
 input_word = gets.chomp
 
 response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+definitions = response.body
 
 response2 = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+pronunciations = response2.body
 
 response3 = Unirest.get("http://api.wordnik.com:80/v4/word.json/cat/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+topExample = response3.body
 
-body = response.body
-body2 = response2.body
-body3 = response3.body
 index = 0
-order = 1
 
-puts body3
-#puts "The first five definitions are #{input_word} are:"
-
-1.times do 
-  puts "#{order}. " + body[index]["text"]
-  puts "#{order}. " + body2[index]["raw"]
-  puts "#{order}. " + body3[index]["text"]
+puts "DEFINITIONS"
+definitions.length.times do 
+  puts "#{index}. #{definitions[index]["text"]}"
+  #puts "#{order}. " + pronunciations[index]["raw"]
+  #puts "#{order}. " + topExample"text"]
   index = index + 1
-  order = order + 1
 end
+
+puts "TOP EXAMPLE"
+puts topExample["text"]
