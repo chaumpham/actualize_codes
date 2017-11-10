419b1986e746293770b341a1d0b7130f6c3bb6df
Chau Pham <theonlychau@gmail.com>
2017-11-09 19:38:47 -0600

    add an each loop

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -9,6 +9,8 @@ input_word = gets.chomp
 response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
 body = response.body
-definition = body["text"]
+body.each do |thing|
+  definition = thing["text"]
+end
 
 puts definition
